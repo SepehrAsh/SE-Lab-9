7749fd99fd1e991b265e76eaf668cd08396d040b
SepehrAshs <sepehrashs@gmail.com>
2022-06-24 23:00:43 +0430

    Refactor 5: Deleting Excessive Comments

--- SE Lab 9/src/main/java/parser/Parser.java
+++ SE Lab 9/src/main/java/parser/Parser.java
@@ -48,11 +48,8 @@ public class Parser {
     while (!finish) {
       try {
         Log.print(/*"lookahead : "+*/ lookAhead.toString() + "\t" + parsStack.peek());
-//                Log.print("state : "+ parsStack.peek());
         currentAction = parseTable.getActionTable(parsStack.peek(), lookAhead);
         Log.print(currentAction.toString());
-        //Log.print("");
-
         switch (currentAction.action) {
           case shift:
             parsStack.push(currentAction.number);
@@ -66,10 +63,8 @@ public class Parser {
             }
 
             Log.print(/*"state : " +*/ parsStack.peek() + "\t" + rule.LHS);
-//                        Log.print("LHS : "+rule.LHS);
             parsStack.push(parseTable.getGotoTable(parsStack.peek(), rule.LHS));
             Log.print(/*"new State : " + */parsStack.peek() + "");
-//                        Log.print("");
             try {
               cg.semanticFunction(rule.semanticAction, lookAhead);
             } catch (Exception e) {
